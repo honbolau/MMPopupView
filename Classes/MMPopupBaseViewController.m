bcf771ede5ca7ca86efe555561f7129f76b01227
恨鐵不成金 <lhb@outlook.com>
2019-11-25 15:09:46 +0800

    Create MMPopupBaseViewController.m

---
+++ Classes/MMPopupBaseViewController.m
@@ -0,0 +1,21 @@
+#import "MMPopupBaseViewController.h"
+
+@interface MMPopupBaseViewController ()
+
+@end
+
+@implementation MMPopupBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+- (BOOL)shouldAutorotate
+{
+    return NO;
+}
+
+@end
